3e8b83a229e72070862999b8811a071a3f28696f
giaphattram <47319655+giaphattram@users.noreply.github.com>
2019-02-21 13:35:03 +0100

    Update Air Quality.sql

--- Air Quality.sql
+++ Air Quality.sql
@@ -2,38 +2,51 @@
 
 #------
 #In which years is the data availalbe?
+#------
 select distinct extract(year from timestamp) as timestamp_year
 from `bigquery-public-data.openaq.global_air_quality`notepad
 order by timestamp_year;
 
 #------
 #In many countries is the data available?
+#------
 select count(distinct country)
 from `bigquery-public-data.openaq.global_air_quality`;
 
 #------
 #What are the difference air pollutants in the dataset?
+#------
 select distinct pollutant
 from `bigquery-public-data.openaq.global_air_quality`;
 
 #------
-#Which air pollutants are most and least prevalent?
-
+#Which air pollutants are most and least prevalent in February 2019?
+#------
 (select pollutant, count(pollutant) as city_count
-from  `bigquery-public-data.openaq.global_air_quality` as a
+from  
+  (select pollutant, timestamp from `bigquery-public-data.openaq.global_air_quality` 
+   where 
+     (extract(year from timestamp)=2019) and 
+     (extract(month from timestamp)=2)
+  ) as a
 group by pollutant 
 order by city_count asc limit 1)
 
-union distinct
+union distinct 
 
 (select pollutant, count(pollutant) as city_count
-from  `bigquery-public-data.openaq.global_air_quality` as a
+from  
+  (select pollutant, timestamp from `bigquery-public-data.openaq.global_air_quality` 
+   where 
+     (extract(year from timestamp)=2019) and 
+     (extract(month from timestamp)=2)
+  ) as a
 group by pollutant 
 order by city_count desc limit 1)
 
 #------
 #For each air pollutant, which are the cities that have the highest level of that pollutant?
-
+#------
 with pollutant_max as
 (
   select pollutant, max(value) as max_value
@@ -46,7 +59,7 @@ where (a.pollutant = b.pollutant) and (a.value = b.max_value);
 
 #------
 #Find the cities with the highest and lowest level of each pollutant in India, Australia, Germany and US.
-
+#------
 with pollutant_max as
 (
   select a.country, a.pollutant, max(a.value) as extremum, 'max' as label
