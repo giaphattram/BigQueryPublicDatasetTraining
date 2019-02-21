05e6c08d343efd8ff4028f60bb812786fd45a7ad
giaphattram <47319655+giaphattram@users.noreply.github.com>
2019-02-21 15:30:31 +0100

    Update Air Quality.sql

--- Air Quality.sql
+++ Air Quality.sql
@@ -72,6 +72,8 @@ where (a.pollutant = b.pollutant) and (a.value = b.max_value);
 #From this, I find out that Baden-Württemberg, the state where I study my Master's degree, has the highest level of CO and O3; 
 #while Nordrhein-Westfalen, the state where I am living and doing my internship, has the lowest level of O3.          
 #The reason is perhaps because BASF is headquartered in Ludwigshafen in Baden-Württemberg.
+#Note: It could be noticed that there are abnormal negative values. Since I couldn't find information whether the negative values
+#actually measure the level of the pollutant, or whether they have some other meanings; I just take them as minimum.
 #------
 with Feb2019 as
 (
